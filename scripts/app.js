a956284967eb62b4448d6647d500a89eb2a64989
Steve Benton <steve@bentonemail.uk>
2026-08-21 18:13:55 +0100

    add initial JS structure

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+// JS Comments Format
+
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+
+editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
