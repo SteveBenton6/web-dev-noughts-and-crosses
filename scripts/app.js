790c455c6f4e3a55282a98c284000e4534b6e646
Steve Benton <steve@bentonemail.uk>
2026-08-24 17:33:08 +0100

    add showing and hiding of modal

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,15 @@
 // JS Comments Format
 
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+
+const cancelConfigButtonElement = document.getElementById("cancel-config-btn");
+
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
+
+cancelConfigButtonElement.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,11 @@
 // JS Comments Format
 
-function openPlayerConfig() {}
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+}
