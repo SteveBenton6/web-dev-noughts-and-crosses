ffeb83a443fc09969bdc24333196ae1805f0e669
Steve Benton <steve@bentonemail.uk>
2026-09-01 15:23:00 +0100

    add start game button functionality

--- scripts/app.js
+++ scripts/app.js
@@ -17,11 +17,14 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
 
 const cancelConfigButtonElement = document.getElementById("cancel-config-btn");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+
+const startNewGameButtonElement = document.getElementById("start-game-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -30,3 +33,5 @@ cancelConfigButtonElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameButtonElement.addEventListener("click", startNewGame);
--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,9 @@
 // JS Comments Format
+
+function startNewGame() {
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set custom player names for both players!");
+    return;
+  }
+  gameAreaElement.style.display = "block";
+}
