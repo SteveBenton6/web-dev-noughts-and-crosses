a214feb3f52b1a5b52ecc890d6b36a06529ea4dd
Steve Benton <steve@bentonemail.uk>
2026-09-02 11:24:39 +0100

    add checking for draw or winner

--- scripts/game.js
+++ scripts/game.js
@@ -34,5 +34,58 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
   console.log(gameData);
 
+  const winnerID = checkForGameOver();
+  console.log(winnerID);
+
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  // Checking the rows for equality
+  for (r = 0; r < 3; r++) {
+    if (
+      gameData[r][0] > 0 &&
+      gameData[r][0] === gameData[r][1] &&
+      gameData[r][1] === gameData[r][2]
+    ) {
+      return gameData[r][0];
+    }
+  }
+
+  // Checking the columns for equality
+  for (c = 0; c < 3; c++) {
+    if (
+      gameData[0][c] > 0 &&
+      gameData[0][c] === gameData[1][c] &&
+      gameData[1][c] === gameData[2][c]
+    ) {
+      return gameData[0][c];
+    }
+  }
+
+  // Checking the diagonal: Top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  // Checking the diagonal: Top right to bottom left
+  if (
+    gameData[0][2] > 0 &&
+    gameData[0][2] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][0]
+  ) {
+    return gameData[2][0];
+  }
+
+  // Checking for draw
+  if (currentRound === 9) {
+    return -1;
+  }
+
+  return 0;
 }
